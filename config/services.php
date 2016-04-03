4887a4efbfc73d1d33ebc33d644499d98b65f915
ejshack <ejshack@iastate.edu>
2016-04-03 00:09:07 -0500

    app name change universal

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => Scheduler\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
